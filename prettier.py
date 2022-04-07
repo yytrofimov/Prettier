2e80a6e39e0a7520963651729cc4eb471901d5e2
yuriy <yytrofimov@yandex.ru>
2022-04-07 19:46:01 +0300

    First commit

---
+++ prettier.py
@@ -0,0 +1,76 @@
+import pprint
+
+
+class Printer:
+    @classmethod
+    def repr(cls, obj):
+        if isinstance(obj, dict):
+            return cls.repr_dict(dict)
+        return pprint.pformat(obj)
+
+    @classmethod
+    def get_dict_lines(cls, obj, indent='\t'):
+        lines = []
+        for index, (key, value) in enumerate(obj.items()):
+            line_prefix = f"{pprint.pformat(key, compact=True)} : "
+            if isinstance(value, dict):
+                dict_lines = cls.get_dict_lines(value, indent)
+                lines.append(line_prefix + dict_lines[0])
+                lines.extend(cls.get_indented_lines(dict_lines[1:], len(line_prefix) * ' '))
+                if index != len(obj) - 1 and len(dict_lines) == 1:
+                    lines[-1] = lines[-1] + ','
+            else:
+                value_lines = pprint.pformat(value, compact=True).split('\n')
+                value_lines = cls.get_indented_lines(value_lines, len(line_prefix) * ' ',
+                                                     skip_first=True)
+                value_lines[0] = line_prefix + value_lines[0]
+                if index != len(obj) - 1 and len(value_lines) == 1:
+                    value_lines[0] = value_lines[0] + ','
+                lines.extend(value_lines)
+        if lines:
+            lines[0] = '{' + lines[0]
+            lines[-1] = lines[-1] + '}'
+            for index in range(1, len(lines)):
+                lines[index] = ' ' + lines[index]
+        else:
+            return ['{}']
+        return lines
+
+    @classmethod
+    def join_lines(cls, lines):
+        return '\n'.join(lines)
+
+    @classmethod
+    def repr_dict(cls, obj):
+        return cls.join_lines(cls.get_dict_lines(obj))
+
+    @classmethod
+    def get_indented_string(cls, obj, indent='\t', indents=1, skip_first=False):
+        lines = cls.get_indented_lines(obj.split('\n'), indent, indents, skip_first)
+        return cls.join_lines(lines)
+
+    @classmethod
+    def get_indented_lines(cls, lines, indent='\t', indents=1, skip_first=False):
+        lines = [_ for _ in lines]
+        for index in range(len(lines)) if skip_first is False else range(1, len(lines)):
+            lines[index] = indent * indents + lines[index]
+        return lines
+
+
+class EndpointPrintMixin:
+    _PRINT_MIXIN_ENDPOINT = None
+
+    def __repr__(self):
+        out = {}
+        if self._PRINT_MIXIN_ENDPOINT is None:
+            endpoint = '__dict__'
+        else:
+            endpoint = self._PRINT_MIXIN_ENDPOINT
+        if not hasattr(self, endpoint):
+            return pprint.pformat(out)
+        attrs = getattr(self, endpoint)
+        for attr in attrs:
+            if not hasattr(self, attr):
+                continue
+            out[attr] = getattr(self, attr)
+        return Printer.repr_dict(out)
