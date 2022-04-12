1e0c51add04e8b8504ef747f8e1e709f476d7df8
yuriy <yytrofimov@yandex.ru>
2022-04-12 12:37:55 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prettier",
+    description='Properly pprint of nested objects',
+    author_email='yytrofimov@gmail.com',
+    url='https://github.com/yytrofimov/Prettier',
+    version="0.0.4",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license_files=('LICENSE.txt',),
+)
